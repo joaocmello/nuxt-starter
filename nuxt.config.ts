532b600d69deeee957b9ec9f195c4a1dade786b5
João Mello <joao.mello.dsg@gmail.com>
2026-02-20 11:00:28 -0300

    Migrate to Neon DB, add server API routes, and update deployment configuration for SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,4 +15,7 @@ export default defineNuxtConfig({
   experimental: { appManifest: false },
   modules: ["@nuxt/icon", "@nuxt/image"],
   compatibilityDate: "2024-12-18",
+  nitro: {
+    preset: 'netlify'
+  }
 });
